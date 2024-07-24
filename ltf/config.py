8e9a58bfd276e2cf0b7c28b42c117793dddbd72c
Tendai <tendai.charura@gmail.com>
2024-07-24 23:04:51 +0700

    Add AvailableTargetLanguages and change .env location

--- ltf/config.py
+++ ltf/config.py
@@ -1,26 +1,23 @@
-from typing import Literal
 from pathlib import Path
+
 from pydantic_settings import BaseSettings
 
+from ltf.models import AvailableTargetLanguages
 
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
+CONFIG_DIR = Path.home() / '.ltf'
 
 
 class Settings(BaseSettings):
-
     # OPENAI settings
     OPENAI_API_KEY: str
     GPT_4o_MINI: str = 'gpt-4o-mini'
     GPT_4o: str = 'gpt-4o'
 
     # Your target language from Language Transfer:
-    TARGET_LANGUAGE: Literal[
-        "Arabic", "French", "German", "Greek",
-        "Italian", "Spanish", "Swahili", "Turkish"
-    ]
+    TARGET_LANGUAGE: AvailableTargetLanguages
 
     class Config:
-        env_file = PROJECT_ROOT / '.env'
+        env_file = CONFIG_DIR / '.env'
 
 
 settings = Settings()
